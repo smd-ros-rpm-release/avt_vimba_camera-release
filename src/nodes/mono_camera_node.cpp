7a1fd5efc932df3addc570abe768498c01dd1457
Miquel Massot <miquel.massot@uib.cat>
2014-11-17 11:53:39 +0100

    Rebase from 'rpm/indigo/avt_vimba_camera'

--- src/nodes/mono_camera_node.cpp
+++ src/nodes/mono_camera_node.cpp
@@ -1,14 +1,14 @@
 #include <ros/ros.h>
-#include <avt_vimba_camera/vimba_ros.h>
+#include <avt_vimba_camera/mono_camera.h>
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "avt_vimba_camera_node");
+	ros::init(argc, argv, "mono_camera_node");
 
   ros::NodeHandle nh;
   ros::NodeHandle nhp("~");
 
-  avt_vimba_camera::VimbaROS vimba_ros(nh,nhp);
+  avt_vimba_camera::MonoCamera mc(nh,nhp);
 
   ros::spin();
   return 0;
